cfd222bbd8d9a25a229bf9111412c0a6e3c0e247
Gabriele dos Santos Oliveira <90703796+santosgabriele@users.noreply.github.com>
2021-12-13 22:08:53 -0300

    cadastroEventos

---
+++ Cadastrodeeventos.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+var senha = 8;
+if (senha >= 8) {
+    console.log("senha aprovada");
+} else {
+    console.log("senha reprovada, favor preenchaa senha com 8 caracteres ");
+}
+console.log(senha);
